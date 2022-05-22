e0c193440cb23b815b80f5e9a365da084884d780
ffonsecaUFP <fmfonseca@ufp.edu.pt>
2022-05-22 21:59:07 +0100

    Initial commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Developed by <strong> TEAM MEMBERS NAMES</strong>
+        </span>
+    </footer>
